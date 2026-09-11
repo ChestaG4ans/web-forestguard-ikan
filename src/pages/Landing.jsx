d7c954b302c662f40a6395bd683a1170e7c6e0f5
Chesta Adabi Karnen <chestaadabikarnen03@gmail.com>
2026-09-12 06:09:59 +0700

    Reorder sponsor logos on landing page: Belmawa, sponsor-1, sponsor-38, sponsor-4, sponsor-5, sponsor-6

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -18,12 +18,12 @@ const BIRDS = [
 ];
 
 const SPONSORS = [
-  { src: '/img/sponsor-6.png', alt: 'Diktisaintek Berdampak', h: 22 },
-  { src: '/img/sponsor-1.png', alt: 'Kemendikbud', h: 30 },
-  { src: '/img/sponsor-4.png', alt: 'PKM', h: 25 },
-  { src: '/img/sponsor-38.png', alt: 'SIMBELMAWA', h: 18 },
   { src: '/img/sponsor-belmawa.png', alt: 'Belmawa', h: 27 },
+  { src: '/img/sponsor-1.png', alt: 'Kemendikbud', h: 30 },
+  { src: '/img/sponsor-38.png', alt: 'SIMBELMAWA', h: 18 },
+  { src: '/img/sponsor-4.png', alt: 'PKM', h: 25 },
   { src: '/img/sponsor-5.png', alt: 'Universitas Gunadarma', h: 30 },
+  { src: '/img/sponsor-6.png', alt: 'Diktisaintek Berdampak', h: 22 },
 ];
 
 function Bird({ left, right, top, w, o, anim }) {
